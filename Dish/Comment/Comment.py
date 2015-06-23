8306e4d21fcd778bb0401edbbb98b628af501ae3
KangShiang <ykshiang@gmail.com>
2015-06-22 20:15:42 -0700

    Bug fixes

--- Dish/Comment/Comment.py
+++ Dish/Comment/Comment.py
@@ -1,6 +1,10 @@
 import webapp2
 
+def put_comment(handler):
+    handler.response.out.write("Put to comment")
+
+def get_comment(handler):
+    handler.response.out.write("Get to comment")
 
 def post_comment(handler):
     handler.response.out.write("Post to comment")
-    return handler
--- Dish/Dish.py
+++ Dish/Dish.py
@@ -6,12 +6,24 @@ import sql as fiidup_sql
 import urlparse
 import json
 from Comment import Comment as CommentHandler
+from Like import Like as LikeHandler
+from Tasted import Tasted as TastedHandler
+from Keep import Keep as KeepHandler
 
-post_sub_routes = {"comment": "post_comment",
-                   "like": "post_like",
-                   "tasted": "post_tasted",
-                   "keep": "post_keep"}
+put_sub_routes = {"PUT_comment": "put_comment",
+                  "PUT_like": "put_like",
+                  "PUT_tasted": "put_tasted",
+                  "PUT_keep": "put_keep"}
 
+get_sub_routes = {"GET_comment": "get_comment",
+                  "GET_like": "get_like",
+                  "GET_tasted": "get_tasted",
+                  "GET_keep": "get_keep"}
+
+post_sub_routes = {"POST_comment": "post_comment",
+                   "POST_like": "post_like",
+                   "POST_tasted": "post_tasted",
+                   "POST_keep": "post_keep"}
 
 class Dish(webapp2.RequestHandler):
     def get(self):
@@ -19,9 +31,39 @@ class Dish(webapp2.RequestHandler):
         if err:
             self.response.out.write(err.message())
             return
-        self.response.out.write(req_params)
+
+        url_string = str(self.request.url)
+        url_obj = urlparse.urlparse(url_string)
+        # str.split returns a list of strings. Google search python str.split for more detail.
+        subdirs = str(url_obj.path).split('/')
+        # Last element in the url
+        last_dir_string = str(subdirs[len(subdirs)-1])
+        num_layers = len(subdirs)
+        if num_layers == 2:
+            self.response.out.write("Retrieve Data")
+            return
+        elif num_layers == 3:
+            try:
+                # Handle the case when the url is /dish/:id
+                int(last_dir_string)
+                # Return info of a specific dish
+            except ValueError:
+                try:
+                    subdir_string = str(subdirs[2])
+                    handling_function = get_sub_routes["GET_" + subdir_string]
+                    getattr(CommentHandler, handling_function)(self)
+                    return
+                except KeyError:
+                    self.response.out.write("Invalid URL")
+                    return
+        self.response.out.write("Invalid URL")
 
     def post(self):
+        err, req_params = utils.validate_data(self.request)
+        if err:
+            self.response.out.write(err.message())
+            return
+
         url_string = str(self.request.url)
         url_obj = urlparse.urlparse(url_string)
         # str.split returns a list of strings. Google search python str.split for more detail.
@@ -30,11 +72,8 @@ class Dish(webapp2.RequestHandler):
         last_dir_string = str(subdirs[len(subdirs)-1])
         num_layers = len(subdirs)
 
+        # Only Handle the case of /dish
         if num_layers == 2 and last_dir_string == "dish":
-            err, req_params = utils.validate_data(self.request)
-            if err:
-                self.response.out.write(err.message())
-                return
             cursor = fiidup_sql.db.cursor()
             try:
                 query = fiidup_sql.get_insert_query_string("dish", req_params)
@@ -50,12 +89,16 @@ class Dish(webapp2.RequestHandler):
             return
         elif num_layers == 3:
             try:
-                handling_function = post_sub_routes[last_dir_string]
-                self = getattr(CommentHandler, handling_function)(self)
+                subdir_string = str(subdirs[2])
+                handling_function = post_sub_routes["POST_" + subdir_string]
+                getattr(CommentHandler, handling_function)(self)
                 return
             except KeyError:
                 self.response.out.write("Invalid URL")
                 return
+        else:
+            self.response.out.write("Invalid URL")
+
 
     def put(self):
         url_string = str(self.request.url)
