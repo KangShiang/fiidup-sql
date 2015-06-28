89900b411743aaabe9610ede6df8290fc60c9064
KangShiang <ykshiang@gmail.com>
2015-06-27 18:15:48 -0700

    bug fix

--- Restaurant/Restaurant.py
+++ Restaurant/Restaurant.py
@@ -14,12 +14,12 @@ from Keep import Keep as keepHandler
 
 put_sub_routes = {"PUT_like": "put_like",
                   "PUT_visited": "put_visited",
-                  "PUT_Review": "put_review",
+                  "PUT_review": "put_review",
                   "PUT_keep": "put_keep"}
 
 get_sub_routes = {"GET_like": "get_like",
                   "GET_visited": "get_visited",
-                  "GET_Review": "get_review",
+                  "GET_review": "get_review",
                   "GET_keep": "get_keep"}
 
 post_sub_routes = {}
--- Restaurant/Review/Review.py
+++ Restaurant/Review/Review.py
@@ -2,18 +2,18 @@ import webapp2
 
 def put_review(handler, id, params):
     if id:
-        handler.response.out.write("Put to comment " + "when id = " + id + " and Param =" + str(params))
+        handler.response.out.write("Put to review " + "when id = " + id + " and Param =" + str(params))
     else:
-        handler.response.out.write("Put to comment")
+        handler.response.out.write("Put to review" + "when id = " + id + " and Param =" + str(params))
 
-def Review(handler, id, params):
+def get_review(handler, id, params):
     if id:
-        handler.response.out.write("Get to comment " + "when id = " + id + " and Param =" + str(params))
+        handler.response.out.write("Get to review " + "when id = " + id + " and Param =" + str(params))
     else:
-        handler.response.out.write("Get to comment")
+        handler.response.out.write("Get to review" + "when id = " + id + " and Param =" + str(params))
 
 def post_review(handler, id, params):
     if id:
-        handler.response.out.write("Post to comment " + "when id = " + id + " and Param =" + str(params))
+        handler.response.out.write("Post to review " + "when id = " + id + " and Param =" + str(params))
     else:
-        handler.response.out.write("Post to comment")
+        handler.response.out.write("Post to review" + "when id = " + id + " and Param =" + str(params))
