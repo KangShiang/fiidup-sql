fe5d8565952b5796015c7e861a4dedfdd2c7e66a
Daniel Tan <daniel.tan@optigo.net>
2015-07-05 17:21:56 -0700

    completed delete, get, and post comment functions

--- Dish/Comment/Comment.py
+++ Dish/Comment/Comment.py
@@ -4,15 +4,40 @@ import sql
 import utils
 import logging
 
-def put_comment(handler, id, params):
+def delete_comment(handler, id, params):
+    # id represents dish_id
+    data = None
+    error = None
+    cursor = sql.db.cursor()
     if id:
-        handler.response.out.write("Put to comment " + "when id = " + id + " and Param =" + str(params))
+        # extract only necessary info
+        comment_id = params.get('comment_id')
+        if comment_id is None:
+            error = "Insufficient info to delete comment"
+            handler.response.status = 403
+        else:
+            try:
+                query = sql.get_delete_query_string('comment', 'comment_id', comment_id)
+                cursor.execute(query)
+                sql.db.commit()
+            except MySQLdb.Error, e:
+                try:
+                    logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    logging.error("MySQL Error: %s" % str(e))
+                    error = "MySQL Error: %s" % str(e)
+                sql.db.rollback()
+                handler.response.status = 403
     else:
+        error = "ID not found"
         handler.response.status = 403
+    cursor.close()
+    return data, error
 
 def get_comment(handler, id, params):
     # id represents dish_id
-    # params is assumed to contain user_id
+    data = None
     error = None
     cursor = sql.db.cursor()
     if id:
@@ -20,7 +45,15 @@ def get_comment(handler, id, params):
             cond = {'dish_id': str(id)}
             query = sql.get_retrieve_query_string(table='comment', cond=cond)
             cursor.execute(query)
-            data = cursor.fetchall()[0]
+            try:
+                datalist = cursor.fetchall()
+                data = []
+                keys = sql.get_column_names('comment')
+                for x in datalist:
+                    x = dict(zip(keys, x))
+                    data.append(x)
+            except IndexError:
+                data = []
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
@@ -29,32 +62,47 @@ def get_comment(handler, id, params):
                 logging.error("MySQL Error: %s" % str(e))
                 error = "MySQL Error: %s" % str(e)
             handler.response.status = 403
-            data = None
-        cursor.execute("DESCRIBE comment")
-        keys = [x[0] for x in cursor.fetchall()]
-        cursor.close()
-        data = dict(zip(keys, data))
-        k = utils.generate_json(handler.request, params.get('user_id'), 'GET', data, error)
-        handler.response.out.write(k)
-        return data, error
     else:
         error = "ID not found"
         handler.response.status = 403
-        return None, error
+    cursor.close()
+    logging.info(data)
+    return data, error
 
+'''
+Returns the comment_id
+'''
 def post_comment(handler, id, params):
     # id represents dish_id
     # input params should have the comment string and user_id of the user who posts the comment
+    data = None
+    error = None
+    cursor = sql.db.cursor()
     if id:
-        table = 'comment'
         # extract only necessary info
         user_id = params.get('user_id')
         comment = params.get('comment')
         dict = {'dish_id': id, 'user_id': user_id, 'comment': comment}
         if (user_id is None) or (comment is None):
+            error = "Insufficient info to post comment"
             handler.response.status = 403
         else:
-            query = sql.get_insert_query_string(table, dict)
-            print params
+            try:
+                query = sql.get_insert_query_string('comment', dict)
+                cursor.execute(query)
+                sql.db.commit()
+                data = {'comment_id': sql.get_last_inserted_pkey(cursor)}
+            except MySQLdb.Error, e:
+                try:
+                    logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    logging.error("MySQL Error: %s" % str(e))
+                    error = "MySQL Error: %s" % str(e)
+                sql.db.rollback()
+                handler.response.status = 403
     else:
+        error = "ID not found"
         handler.response.status = 403
+    cursor.close()
+    return data, error
