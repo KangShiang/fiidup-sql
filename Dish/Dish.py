23c7a0f0baebd22ad17104b71b26d70bac4c661a
KangShiang <ykshiang@gmail.com>
2015-07-05 17:12:53 -0700

    GET dishes implemented

--- Dish/Dish.py
+++ Dish/Dish.py
@@ -27,6 +27,8 @@ post_sub_routes = {"POST_comment": "post_comment",
 
 class Dish(webapp2.RequestHandler):
     def get(self):
+        data = None
+        error = None
         err, req_params = utils.validate_data(self.request)
         if err:
             self.response.out.write(err.message())
@@ -39,27 +41,26 @@ class Dish(webapp2.RequestHandler):
         # Last element in the url
         last_dir_string = str(subdirs[len(subdirs)-1])
         num_layers = len(subdirs)
+
         if num_layers == 2:
-            dishHandler.get_dish(self, None, req_params)
+            data, error = dishHandler.get_dish(self, None, req_params)
         elif num_layers == 3:
             try:
                 # Handle the case when the url is /dish/<id>
                 int(last_dir_string)
-                dish, error = dishHandler.get_dish(self, last_dir_string, req_params)
-                logging.info(dish)
+                data, error = dishHandler.get_dish(self, last_dir_string, req_params)
+                logging.info(data)
                 logging.info(error)
-                json = utils.generate_json(self.request, 123, "GET", dish, error)
-                # self.response.out.write()
-                return
+
             except ValueError:
                 try:
                     subdir_string = str(subdirs[2])
                     handling_function = get_sub_routes["GET_" + subdir_string]
                     getattr(globals()[subdir_string + "Handler"], handling_function)(self, None, req_params)
-                    return
+
                 except KeyError:
                     self.response.status = 405
-                    return
+
         elif num_layers == 4:
             try:
                 # Handle the case when the url is /dish/<info>:id
@@ -70,9 +71,11 @@ class Dish(webapp2.RequestHandler):
                 # Return info of a specific dish
             except KeyError:
                 self.response.status = 405
-                return
+
         else:
             self.response.status = 405
+        self.response.out.write(utils.generate_json(self.request, 123, "GET", data, error))
+
 
     def post(self):
         '''
@@ -119,7 +122,9 @@ class Dish(webapp2.RequestHandler):
             self.response.status = 405
 
     def put(self):
-        authenticated, user = utils.process_cookie(self.request, self.response)
+        data = None
+        error = None
+        #authenticated, user = utils.process_cookie(self.request, self.response)
         # if not authenticated:
         #     return
         err, req_params = utils.validate_data(self.request)
@@ -144,15 +149,14 @@ class Dish(webapp2.RequestHandler):
             return
 
         if num_layers == 3:
-            dishHandler.put_dish(self, last_dir_string, req_params)
-            return
+            data, error = dishHandler.put_dish(self, last_dir_string, req_params)
         elif num_layers == 4:
             try:
                 subdir_string = str(subdirs[2])
                 handling_function = put_sub_routes["PUT_" + subdir_string]
                 getattr(globals()[subdir_string + "Handler"], handling_function)(self, last_dir_string, req_params)
-                return
             except KeyError:
                 self.response.status = 405
-                return
-        self.response.status = 405
+        else:
+            self.response.status = 405
+        self.response.out.write(utils.generate_json(self.request, 123, "GET", data, error))
--- Dish/dishHandler.py
+++ Dish/dishHandler.py
@@ -18,6 +18,10 @@ def put_dish(handler, id, params):
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                if e.args[0] == 1054:
+                    handler.response.status = 403
+                    error = "Invalid Argument"
+                    return params, error
                 error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
             except IndexError:
                 logging.error("MySQL Error: %s" % str(e))
@@ -34,29 +38,35 @@ def put_dish(handler, id, params):
 
 def get_dish(handler, id, params):
     error = None
+    dish = []
     if id:
         cursor = fiidup_sql.db.cursor()
         try:
             condition = {"dish_id": id}
             query_string = fiidup_sql.get_retrieve_query_string(table="dish", cond=condition, limit=1)
             cursor.execute(query_string)
-            values = cursor.fetchall()[0]
-            new_values = []
-            for x in values:
-                try:
-                    int(x)
-                    new_values.append(x)
-                except (ValueError):
-                    new_values.append(cgi.escape(x))
-
-            cursor.execute("DESCRIBE %s" % "dish;")
-            keys = [x[0] for x in cursor.fetchall()]
-            dish = dict(zip(keys, new_values))
-            logging.info(dish)
-            return dish, error
+            try:
+                values = cursor.fetchall()
+                new_values = []
+                for x in values:
+                    try:
+                        int(x)
+                        new_values.append(x)
+                    except (ValueError):
+                        new_values.append(cgi.escape(x))
+                cursor.execute("DESCRIBE %s" % "dish;")
+                keys = [x[0] for x in cursor.fetchall()]
+                dish = dict(zip(keys, new_values))
+                logging.info(dish)
+                return dish, error
+            except IndexError:
+                return dish, None
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                if e.args[0] == 1054:
+                    error = "Invalid Argument"
+                    return None , error
                 error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
             except IndexError:
                 logging.error("MySQL Error: %s" % str(e))
@@ -65,7 +75,53 @@ def get_dish(handler, id, params):
             return None, error
         cursor.close()
     else:
-        handler.response.out.write("Get to dish" + " and Param =" + str(params))
+        cursor = fiidup_sql.db.cursor()
+        if params.get("count"):
+            count = params.get("count")
+            del params['count']
+        else:
+            count = 10
+
+        logging.info("GET DISH WITH COUNT", count)
+
+        try:
+            condition = params
+            query_string = fiidup_sql.get_retrieve_numeric_query_string(table="dish", cond=condition, limit=count)
+            cursor.execute(query_string)
+            try:
+                values = cursor.fetchall()
+                cursor.execute("DESCRIBE %s" % "dish;")
+                keys = [x[0] for x in cursor.fetchall()]
+                logging.info(values)
+                dish_list = []
+                for list in values:
+                    new_values = []
+                    for x in list:
+                        try:
+                            int(x)
+                            new_values.append(x)
+                        except (ValueError):
+                            new_values.append(cgi.escape(x))
+                    dish = dict(zip(keys, new_values))
+                    dish_list.append(dish)
+                logging.info(dish_list)
+                return dish_list, error
+            except IndexError:
+                return dish, None
+        except MySQLdb.Error, e:
+            try:
+                logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                if e.args[0] == 1054:
+                    error = "Invalid Argument"
+                    return None , error
+                error = "Invalid Syntax"
+            except IndexError:
+                logging.error("MySQL Error: %s" % str(e))
+                error = "MySQL Error: %s" % str(e)
+            handler.response.status = 403
+            return None, error
+        cursor.close()
+        # handler.response.out.write("Get to dish" + " and Param =" + str(params))
 
 
 def post_dish(handler, id, params):
