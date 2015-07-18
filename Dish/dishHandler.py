2cd6a2ad6c828af09094d3fadfc19fdd833ea709
Daniel Tan <daniel.tan@optigo.net>
2015-07-18 14:52:08 -0700

    some refactorings

--- Dish/dishHandler.py
+++ Dish/dishHandler.py
@@ -2,45 +2,40 @@ import webapp2
 import MySQLdb
 import sql as fiidup_sql
 import logging
-import json
 import cgi
-import utils
 
 def put_dish(handler, id, params):
+    data = None
     error = None
+    cursor = fiidup_sql.db.cursor()
     if id:
-        cursor = fiidup_sql.db.cursor()
         try:
             query_string = fiidup_sql.get_modify_query_string("dish", params, "dish_id", id)
             cursor.execute(query_string)
             fiidup_sql.db.commit()
-            return params, error
+            data = params
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
                 if e.args[0] == 1054:
-                    handler.response.status = 403
                     error = "Invalid Argument"
-                    return params, error
-                error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                else:
+                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
             except IndexError:
                 logging.error("MySQL Error: %s" % str(e))
                 error = "MySQL Error: %s" % str(e)
-
             handler.response.status = 403
-            return None, error
-        cursor.close()
     else:
         error = "ID not found"
         handler.response.status = 403
-        return None, error
-
+    cursor.close()
+    return data, error
 
 def get_dish(handler, id, params):
     error = None
-    dish = []
+    dish = None
+    cursor = fiidup_sql.db.cursor()
     if id:
-        cursor = fiidup_sql.db.cursor()
         try:
             condition = {"dish_id": id}
             query_string = fiidup_sql.get_retrieve_query_string(table="dish", cond=condition, limit=1)
@@ -52,38 +47,32 @@ def get_dish(handler, id, params):
                     try:
                         int(x)
                         new_values.append(x)
-                    except (ValueError):
+                    except ValueError:
                         new_values.append(cgi.escape(x))
                 cursor.execute("DESCRIBE %s" % "dish;")
                 keys = [x[0] for x in cursor.fetchall()]
                 dish = dict(zip(keys, new_values))
                 logging.info(dish)
-                return dish, error
             except IndexError:
-                return dish, None
+                pass
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
                 if e.args[0] == 1054:
                     error = "Invalid Argument"
-                    return None , error
-                error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                else:
+                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
             except IndexError:
                 logging.error("MySQL Error: %s" % str(e))
                 error = "MySQL Error: %s" % str(e)
             handler.response.status = 403
-            return None, error
-        cursor.close()
     else:
-        cursor = fiidup_sql.db.cursor()
         if params.get("count"):
             count = params.get("count")
             del params['count']
         else:
             count = 10
-
         logging.info("GET DISH WITH COUNT", count)
-
         try:
             condition = params
             query_string = fiidup_sql.get_retrieve_numeric_query_string(table="dish", cond=condition, limit=count)
@@ -100,29 +89,27 @@ def get_dish(handler, id, params):
                         try:
                             int(x)
                             new_values.append(x)
-                        except (ValueError):
+                        except ValueError:
                             new_values.append(cgi.escape(x))
                     dish = dict(zip(keys, new_values))
                     dish_list.append(dish)
                 logging.info(dish_list)
-                return dish_list, error
+                dish = dish_list
             except IndexError:
-                return dish, None
+                pass
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
                 if e.args[0] == 1054:
                     error = "Invalid Argument"
-                    return None , error
-                error = "Invalid Syntax"
+                else:
+                    error = "Invalid Syntax"
             except IndexError:
                 logging.error("MySQL Error: %s" % str(e))
                 error = "MySQL Error: %s" % str(e)
             handler.response.status = 403
-            return None, error
-        cursor.close()
-        # handler.response.out.write("Get to dish" + " and Param =" + str(params))
-
+    cursor.close()
+    return dish, error
 
 def post_dish(handler, id, params):
     data = None
@@ -137,7 +124,6 @@ def post_dish(handler, id, params):
             cursor.execute(query)
             fiidup_sql.db.commit()
             data = params
-            return data, error
         except MySQLdb.Error, e:
             try:
                 logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
