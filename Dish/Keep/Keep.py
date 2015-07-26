5a51fbc2dbb203087d62a8c8a5a56ba9d44fba20
Daniel Tan <daniel.tan@optigo.net>
2015-07-25 21:43:24 -0700

    completed keep for dish: get and put

--- Dish/Keep/Keep.py
+++ Dish/Keep/Keep.py
@@ -1,19 +1,110 @@
-import webapp2
+import sql
+import logging
+import MySQLdb
 
 def put_keep(handler, id, params):
+    # id represents dish_id
+    # params should have the 'user_id' and 'keep' fields
+    data = None
+    error = None
+    user_list = None
+    cursor = sql.db.cursor()
     if id:
-        handler.response.out.write("Put to keep " + "when id = " + id + " and Param =" + str(params))
+        # extract only necessary info
+        # encode user_id as ascii string
+        user_id = params.get('user_id').encode('ascii')
+        keep = params.get('keep')
+        dict = {'dish_id': id, 'user_id': user_id}
+        if user_id is None:
+            error = "Insufficient info to update dish_keep"
+            handler.response.status = 403
+        else:
+            # query dish_keep based on dish_id for user_id
+            try:
+                cond = {'dish_id': str(id)}
+                query = sql.get_retrieve_query_string(table='dish_keep', cond=cond)
+                cursor.execute(query)
+                # expects only one result: ((dish_id, 'user_list'),)
+                # user_list is a string
+                user_list = cursor.fetchall()[0][1]
+                user_list = user_list.strip('[]').split(', ')
+                # if keep is True (user keeps this dish)
+                # append user_id
+                # increase keep_count in dish table
+                if keep == 'True' and user_id not in user_list:
+                    logging.info("keep")
+                    user_list.append(user_id)
+                # if keep is False (user unkeep this dish)
+                # remove user_id
+                # decrease keep_count in dish table
+                elif keep == 'False' and user_id in user_list:
+                    logging.info("unkeep")
+                    user_list.remove(user_id)
+                else:
+                    # assuming keep will only hold values: true or false - ignore otherwise
+                    logging.info("Not an error: May just be inconsistency in UI and database")
+            except MySQLdb.Error, e:
+                try:
+                    logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    logging.error("MySQL Error: %s" % str(e))
+                    error = "MySQL Error: %s" % str(e)
+                handler.response.status = 403
+
+            # convert user_list to a string, remove "'" from individual elements
+            user_list = str(user_list).replace("'", '')
+            # modify entry in database: entry should exist upon dish creation
+            logging.info(user_list)
+            try:
+                query = sql.get_modify_query_string(table='dish_keep', params={'user_id': user_list},
+                                                    primary_key='dish_id', id=id)
+                cursor.execute(query)
+                sql.db.commit()
+                data = {'dish_id': id}
+            except MySQLdb.Error, e:
+                try:
+                    logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    logging.error("MySQL Error: %s" % str(e))
+                    error = "MySQL Error: %s" % str(e)
+                sql.db.rollback()
+                handler.response.status = 403
     else:
-        handler.response.out.write("Put to keep")
+        error = "ID not found"
+        handler.response.status = 403
+    cursor.close()
+    return data, error
 
 def get_keep(handler, id, params):
+    # id represents dish_id
+    data = None
+    error = None
+    cursor = sql.db.cursor()
     if id:
-        handler.response.out.write("Get to keep " + "when id = " + id + " and Param =" + str(params))
+        try:
+            cond = {'dish_id': str(id)}
+            query = sql.get_retrieve_query_string(table='dish_keep', cond=cond)
+            cursor.execute(query)
+            datalist = cursor.fetchall()
+            data = []
+            keys = sql.get_column_names('dish_keep')
+            for x in datalist:
+                x = dict(zip(keys, x))
+                data.append(x)
+            logging.info(data)
+        except MySQLdb.Error, e:
+            try:
+                logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+            except IndexError:
+                logging.error("MySQL Error: %s" % str(e))
+                error = "MySQL Error: %s" % str(e)
+            handler.response.status = 403
     else:
-        handler.response.out.write("Get to keep")
-
-def post_keep(handler, id, params):
-    if id:
-        handler.response.out.write("Post to keep " + "when id = " + id + " and Param =" + str(params))
-    else:
-        handler.response.out.write("Post to keep")
+        error = "ID not found"
+        handler.response.status = 403
+    cursor.close()
+    logging.info(data)
+    return data, error
