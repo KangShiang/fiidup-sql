57a44589948f89a51fc431070ed18467e37c2be8
Daniel Tan <daniel.tan@optigo.net>
2015-07-26 00:59:50 -0700

    fully completed dish keep, like and tasted, proper fix for 1st element in user_id

--- Dish/Keep/Keep.py
+++ Dish/Keep/Keep.py
@@ -7,7 +7,7 @@ def put_keep(handler, id, params):
     # params should have the 'user_id' and 'keep' fields
     data = None
     error = None
-    user_list = None
+    change = None
     cursor = sql.db.cursor()
     if id:
         # extract only necessary info
@@ -31,17 +31,18 @@ def put_keep(handler, id, params):
                 # expects only one result: ((dish_id, 'user_list'),)
                 # user_list is a string
                 user_list = cursor.fetchall()[0][1]
-                user_list = list() if user_list == '' else user_list.strip('[]').split(', ')
+                user_list = user_list.strip('[]').split(', ')
+                if user_list[0] == '':
+                    user_list = list()
                 # keep is True (user keeps this dish)
                 # append user_id
                 if user_id not in user_list:
                     logging.info("keep")
                     user_list.append(user_id)
-                # if keep is False (shouldn't be able to unkeep)
+                    change = '+'
                 else:
                     # user_id already in the list, don't update table
                     logging.info("Not an error: May just be inconsistency in UI and database")
-                    user_list = None
                     data = {'dish_id': id}
             except MySQLdb.Error, e:
                 try:
@@ -52,7 +53,7 @@ def put_keep(handler, id, params):
                     error = "MySQL Error: %s" % str(e)
                 handler.response.status = 403
 
-            if user_list is not None:
+            if change is not None:
                 # convert user_list to a string, remove "'" from individual elements
                 user_list = str(user_list).replace("'", '')
                 # modify entry in database: entry should exist upon dish creation
@@ -62,8 +63,8 @@ def put_keep(handler, id, params):
                                                         primary_key='dish_id', id=id)
                     cursor.execute(query)
                     # increase keep_count in dish table
-                    query = sql.get_modify_query_string(table='dish', params={'keep_count': 'keep_count + 1'},
-                                                        primary_key='dish_id', id=id)
+                    param = {'keep_count': 'keep_count %s 1' % change}
+                    query = sql.get_modify_query_string(table='dish', params=param, primary_key='dish_id', id=id)
                     cursor.execute(query)
                     sql.db.commit()
                     data = {'dish_id': id}
--- Dish/Like/Like.py
+++ Dish/Like/Like.py
@@ -7,7 +7,7 @@ def put_like(handler, id, params):
     # params should have the 'user_id' and 'like' fields
     data = None
     error = None
-    user_list = None
+    change = None
     cursor = sql.db.cursor()
     if id:
         # extract only necessary info
@@ -29,21 +29,26 @@ def put_like(handler, id, params):
                 # user_list is a string
                 user_list = cursor.fetchall()[0][1]
                 user_list = user_list.strip('[]').split(', ')
+                if user_list[0] == '':
+                    user_list = list()
                 # if like is True (user likes this dish)
                 # append user_id
                 # increase like_count in dish table
                 if like == 'True' and user_id not in user_list:
                     logging.info("like")
                     user_list.append(user_id)
+                    change = '+'
                 # if like is False (user unlike this dish)
                 # remove user_id
                 # decrease like_count in dish table
                 elif like == 'False' and user_id in user_list:
                     logging.info("unlike")
                     user_list.remove(user_id)
+                    change = '-'
                 else:
-                    # assuming like will only hold values: true or false - ignore otherwise
+                    # assuming like will only hold values: true or false, don't update table
                     logging.info("Not an error: May just be inconsistency in UI and database")
+                    data = {'dish_id': id}
             except MySQLdb.Error, e:
                 try:
                     logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
@@ -53,25 +58,30 @@ def put_like(handler, id, params):
                     error = "MySQL Error: %s" % str(e)
                 handler.response.status = 403
 
-            # convert user_list to a string, remove "'" from individual elements
-            user_list = str(user_list).replace("'", '')
-            # modify entry in database: entry should exist upon dish creation
-            logging.info(user_list)
-            try:
-                query = sql.get_modify_query_string(table='dish_like', params={'user_id': user_list},
-                                                    primary_key='dish_id', id=id)
-                cursor.execute(query)
-                sql.db.commit()
-                data = {'dish_id': id}
-            except MySQLdb.Error, e:
+            if change is not None:
+                # convert user_list to a string, remove "'" from individual elements
+                user_list = str(user_list).replace("'", '')
+                # modify entry in database: entry should exist upon dish creation
+                logging.info(user_list)
                 try:
-                    logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-                    error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                except IndexError:
-                    logging.error("MySQL Error: %s" % str(e))
-                    error = "MySQL Error: %s" % str(e)
-                sql.db.rollback()
-                handler.response.status = 403
+                    query = sql.get_modify_query_string(table='dish_like', params={'user_id': user_list},
+                                                        primary_key='dish_id', id=id)
+                    cursor.execute(query)
+                    # change like_count in dish table
+                    param = {'like_count': 'like_count %s 1' % change}
+                    query = sql.get_modify_query_string(table='dish', params=param, primary_key='dish_id', id=id)
+                    cursor.execute(query)
+                    sql.db.commit()
+                    data = {'dish_id': id}
+                except MySQLdb.Error, e:
+                    try:
+                        logging.error("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+                        error = "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                    except IndexError:
+                        logging.error("MySQL Error: %s" % str(e))
+                        error = "MySQL Error: %s" % str(e)
+                    sql.db.rollback()
+                    handler.response.status = 403
     else:
         error = "ID not found"
         handler.response.status = 403
