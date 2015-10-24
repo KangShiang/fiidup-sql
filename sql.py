7e6b4111c501bb090e1aaf555a4c1db6e7b9c7fc
Daniel Tan <daniel.tan@optigo.net>
2015-10-24 16:08:15 -0700

    docstring

--- sql.py
+++ sql.py
@@ -2,7 +2,8 @@ import re
 import logging
 import MySQLdb
 
-db = MySQLdb.connect(host='173.194.107.106', port=3306, db='fiidup_main', user='all')
+#db = MySQLdb.connect(host='173.194.107.106', port=3306, db='fiidup_main', user='all')
+db = MySQLdb.connect(host='localhost', port=3306, db='fiidup_main', user='root', passwd='root')
 
 def is_int(x):
     try:
@@ -36,17 +37,17 @@ def get_insert_query_string(table, params):
     logging.info(query)
     return query
 
-'''
-Function to query data from the database, with exact matching conditions
-table is the table to query FROM
-params if specified, should have a list of columns to SELECT
-eg: params = ['dish_name', 'restaurant_id']
-if params is not specified or is empty, SELECT *
-cond if specified, should have a dictionary of key, value pairs as the WHERE clause
-limit if specified, indicates the LIMIT in integer, default None
-eg: cond = {'dish_name': 'Cake', 'like_count': 0, 'dish_id': [123, 345]}
-'''
 def get_retrieve_query_string(table, params=None, cond=None, limit=None):
+    """
+    Function to query data from the database, with exact matching conditions
+    table is the table to query FROM
+    params if specified, should have a list of columns to SELECT
+    eg: params = ['dish_name', 'restaurant_id']
+    if params is not specified or is empty, SELECT *
+    cond if specified, should have a dictionary of key, value pairs as the WHERE clause
+    limit if specified, indicates the LIMIT in integer, default None
+    eg: cond = {'dish_name': 'Cake', 'like_count': 0, 'dish_id': [123, 345]}
+    """
     logging.info(params)
     query = "SELECT "
     if (params is None) or len(params) == 0:
@@ -84,18 +85,18 @@ def get_retrieve_query_string(table, params=None, cond=None, limit=None):
     logging.info(query)
     return query
 
-'''
-Function to query data from the database, with numeric operations
-table is the table to query FROM
-params if specified, should have a list of columns to SELECT
-eg: params = ['dish_name', 'restaurant_id']
-if params is not specified or is empty, SELECT *
-cond if specified, should have a dictionary of key, value pairs as the WHERE clause
-cond must be numeric, contains the operator, formatted as a string
-limit if specified, indicates the LIMIT, default None
-eg: cond = {'like_count': '< 10', 'comment_count': '> 20', 'tasted_count': '= 50'}
-'''
 def get_retrieve_numeric_query_string(table, params=None, cond=None, limit=None):
+    """
+    Function to query data from the database, with numeric operations
+    table is the table to query FROM
+    params if specified, should have a list of columns to SELECT
+    eg: params = ['dish_name', 'restaurant_id']
+    if params is not specified or is empty, SELECT *
+    cond if specified, should have a dictionary of key, value pairs as the WHERE clause
+    cond must be numeric, contains the operator, formatted as a string
+    limit if specified, indicates the LIMIT, default None
+    eg: cond = {'like_count': '< 10', 'comment_count': '> 20', 'tasted_count': '= 50'}
+    """
     logging.info(params)
     query = "SELECT "
     if (params is None) or len(params) == 0:
@@ -121,16 +122,16 @@ def get_retrieve_numeric_query_string(table, params=None, cond=None, limit=None)
     logging.info(query)
     return query
 
-'''
-Function to generate 1 dictionary key, value pair which can be added to cond
-* use dict.update(dict2) to update existing dict
-axis either 'X' or 'Y'; specifies longitude or latitude
-min and max are signed integers
-Returns one dictionary element
-eg: {'X(location)': 'BETWEEN 25 AND 50'}
-If axis, min or max is invalid, None will be returned
-'''
 def generate_location_range(axis, min, max):
+    """
+    Function to generate 1 dictionary key, value pair which can be added to cond
+    * use dict.update(dict2) to update existing dict
+    axis either 'X' or 'Y'; specifies longitude or latitude
+    min and max are signed integers
+    Returns one dictionary element
+    eg: {'X(location)': 'BETWEEN 25 AND 50'}
+    If axis, min or max is invalid, None will be returned
+    """
     if min > max:
         return None
     temp = 'BETWEEN ' + str(min) + ' AND ' + str(max)
@@ -144,17 +145,21 @@ def generate_location_range(axis, min, max):
             return {'Y(location)': temp}
     return None
 
-'''
-Function to generate a query string which updates an entry in the table
-based on the primary_key
-'''
 def get_modify_query_string(table, params, primary_key, id):
+    """
+    Function to generate a query string which updates an entry in the table
+    based on the primary_key
+    :type table: str
+    :type params: dict
+    :type primary_key: str
+    :type id: str
+    """
     query = "UPDATE " + table + " SET "
     for key, value in params.iteritems():
         if key != primary_key:
             if re.search('[+-]', str(value)) is not None:
                 query = query + key + "=" + str(value) + ", "
-            elif is_int(value):
+            elif type(value) is int:
                 query = query + key + "=" + value + ", "
             else:
                 if 'GeomFromText' in value:
@@ -165,20 +170,20 @@ def get_modify_query_string(table, params, primary_key, id):
     logging.info(query)
     return query
 
-'''
-Function to generate a query string which deletes an entry in the table
-based on the primary_key and the corresponding value
-'''
 def get_delete_query_string(table, primary_key, value):
+    """
+    Function to generate a query string which deletes an entry in the table
+    based on the primary_key and the corresponding value
+    """
     query = "DELETE FROM " + str(table)
     query += " WHERE " + str(primary_key) + " = " + str(value)
     logging.info(query)
     return query
 
-'''
-Function to return the entire list of column names from input table
-'''
 def get_column_names(table):
+    """
+    Function to return the entire list of column names from input table
+    """
     cursor = db.cursor()
     cursor.execute("DESCRIBE %s;" % table)
     keys = [x[0] for x in cursor.fetchall()]
